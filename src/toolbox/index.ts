25854dd1934af8b72846bc1d33293ae62ce3e3c3
aoteron <153718746+aoteron@users.noreply.github.com>
2024-02-21 18:14:01 +0100

    Add 'toolbox' folder and 'index.ts' file
    This commit introduces a new 'toolbox' folder to contain utility functions crucial for our application. These functions are aimed at enhancing code cleanliness and organization without directly impacting the core business logic. The 'index.ts' file within the 'toolbox' folder contains various utility functions, such as 'setErrorInput', 'setSuccessInput', 'resetInput', 'resetForm', and 'getUsers'.
    
    Each utility function serves a specific purpose, contributing to the overall clarity and maintainability of the codebase. Additionally, the 'getUsers' function retrieves a list of simulated users, providing essential data for testing and development purposes.
    
    This commit aligns with best practices for commit messages, providing clear and concise information about the changes introduced. By organizing utility functions within the 'toolbox' folder, we improve the structure of our project and facilitate future development efforts.

---
+++ src/toolbox/index.ts
@@ -0,0 +1,63 @@
+/* Here we have a file containing a collection of utility functions crucial for our app.
+*These functions enhance code cleanliness and organization, without directly impacting the core business logic.
+*For instance, we might include date formatting, currency conversion, and object data extraction.
+*By leveraging these utilities, we streamline our codebase, ensuring clarity and maintainability. */
+
+// Sets an 'error' state on an HTML input element.
+export function setErrorInput(input: Element) {
+    input.classList.add('error');
+    input.classList.remove('success');
+    input.nextElementSibling?.classList.remove('hide');
+  }
+  
+  // Sets a 'success' state on an HTML input element.
+  export function setSuccessInput(input: Element) {
+    input.classList.remove('error');
+    input.classList.add('success');
+    input.nextElementSibling?.classList.add('hide');
+  }
+  
+  // Resets the state of an HTML input element to its default state.
+  export function resetInput(input: Element) {
+    input.classList.remove('error');
+    input.classList.remove('success');
+    input.nextElementSibling?.classList.add('hide');
+  }
+  
+  // Resets multiple HTML input elements to their default state.
+  export function resetForm(inputs: HTMLInputElement[]) { // --- CAMBIADO DE ELEMENT A HTMLINPUTELEMENT --- //
+    inputs.forEach((input) => {
+      input.value = '';
+      resetInput(input);
+    });
+  }
+  
+  // Returns a list of simulated users.
+export async function getUsers() {
+    return [
+      {
+        id: "1",
+        Name: "Anna",
+        password: "pass123",
+        cart: [],
+        wishlist: [],
+        email: "anna@example.com",
+      },
+      {
+        id: "2",
+        Name: "Brian",
+        password: "pass456",
+        cart: [],
+        wishlist: [],
+        email: "brian@example.com",
+      },
+      {
+        id: "3",
+        Name: "Catherine",
+        password: "pass789",
+        cart: [],
+        wishlist: [],
+        email: "catherine@example.com",
+      },
+    ];
+  }  
