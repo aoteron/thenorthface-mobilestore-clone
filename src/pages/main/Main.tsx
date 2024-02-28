ef54eb23e20c7f1156bdef6ae44bcd154e22cf3b
aoteron <153718746+aoteron@users.noreply.github.com>
2024-02-28 13:44:21 +0100

    feat product swipper
    Add product swipper into Main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -25,49 +25,38 @@ const Main: React.FC = () => {
         <section className='flex flex-col items-center mt-6'>
           <h3 className='flex justify-center text-x18 font-semibold tracking-wide uppercase'>Introducing Ripstop Icons</h3>
           <p className='text-x14 font-light mt-2'>Where needs met creativity. Innovation from the 70's</p>
-          <button className='bg-primary text-secondary text-x16 font-semibold hover:text-tertiary transition-colors duration-200 ease-in cursor-pointer uppercase mt-6 p-2 pl-6 pr-6 mb-14'>Look at the new collection</button>
+          <button className='bg-primary text-secondary text-x16 font-semibold hover:text-tertiary transition-colors duration-200 ease-in cursor-pointer uppercase mt-6 p-2 pl-6 pr-6 mb-10'>Look at the new collection</button>
         </section>
       </div>
 
       <Swiper
-        slidesPerView={4}
+        slidesPerView={3}
         centeredSlides={true}
-        spaceBetween={10}
+        spaceBetween={5}
         grabCursor={true}
         pagination={{
           clickable: true,
         }}
         modules={[Pagination]}
-        className="mySwiper "
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-
-      <section className="flex justify-center flex-wrap">
-        {products.map(product => {
-          const firstVariation = product.variations[0];
-
-          return (
-            <div key={product.id} className="flex flex-row p-4">
-              <img src={firstVariation.image}
+        className=""
+        >
+      {products.map(product => {
+        const firstVariation = product.variations[0];
+    
+        return (
+          <SwiperSlide key={product.id} className="flex flex-col items-center">
+            <img
+              src={firstVariation.image}
               alt={`Imagen de la categoría ${product.category}`}
-              style={{ width: '25%' }}
-              />
-              <Link to={`/product/${product.id}`} key={product.id}>
-              <h3>{product.category}</h3>
-              </Link>
-            </div>
-          );
-        })}
-      </section>
+              className=""
+            />
+            <Link to={`/product/${product.id}`}>
+              <h3 className='mt-6 mb-14 uppercase text-lg underline font-semibold'>{product.category}</h3>
+            </Link>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
     </div>
   );
 };
